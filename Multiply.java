86ceddcc061a75a3ee87e14b5511d820b5485af0
shawshank-01 <shashank.0901sharma@gmail.com>
2021-10-20 17:18:18 +0530

    Added print statement to Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -2,6 +2,6 @@ public class multiply{
   public static void main (String[] args){
     int a = 5;
     int b = 10;
-    System.out.println(a * b);
+    System.out.println("Multiplication: " + (a * b));
   }
 }
