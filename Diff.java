637934353e8bb870db815400f9371d1cf2084340
shawshank-01 <shashank.0901sharma@gmail.com>
2021-10-20 16:42:15 +0530

    added print statement to Sum.java and created Diff.java

--- Diff.java
+++ Diff.java
@@ -2,6 +2,6 @@ public class sum{
   public static void main (String[] args){
     int a = 5;
     int b = 10;
-    System.out.println("Sum: " + a + b);
+    System.out.println("Difference : " + (a - b));
   }
 }
